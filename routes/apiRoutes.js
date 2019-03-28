f359acf58f8c524906b0d339261c3b72d8dbadd3
Sabrina <zsbrina20@gmail.com>
2019-03-27 20:48:54 -0500

    .

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,18 +1,27 @@
 var db = require("../models");
 
 module.exports = function(app) {
-  // Get all examples
-  // app.get("/api/users/:age", function(req, res) {
-  //   // db.Example.findAll({where: { [Example.age]: [req.params.age+ 5,req.params.age- 5] }}).then(function(dbExamples) {
-  //   //   res.json(dbExamples);
-  //   // });
-  //   res.json({test})
-  // });
+ // Get all examples
+ // app.get("/api/examples", function(req, res) {
 
-  // Create a new example
-  app.post("/api/users", function(req, res) {
-    db.Example.create(req.body).then(function(dbMatch) {
-      res.json(dbMatch);
-    });
-  });
-};
+
+ //   db.Example.findAll({}).then(function(dbExamples) {
+ //     res.json(dbExamples);
+ //   });
+ // });
+
+
+
+ // Create a new example
+ app.post("/api/match", function(req, res) {
+   db.Friend.create(req.body).then(function(dbmatchfinder) {
+     res.json(dbmatchfinder);
+ // app.get("/api/users/:age", function(req, res) {
+ //   // db.Example.findAll({where: { [Example.age]: [req.params.age+ 5,req.params.age- 5] }}).then(function(dbExamples) {
+ //   //   res.json(dbExamples);
+ //   // });
+ //   res.json({test})
+ // });
+   });
+ });
+}
