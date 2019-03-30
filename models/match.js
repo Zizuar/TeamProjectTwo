ee951a3791173adf8cb4e8e6f3f2215e2babe1b9
Zizuar <zizuar@comcast.net>
2019-03-29 20:18:31 -0500

    Add orefernce for model and get for matches.

--- models/match.js
+++ models/match.js
@@ -11,7 +11,7 @@ export default function (sequelize, DataTypes) {
     created_at: DataTypes.DATE,
     // eslint-disable-next-line camelcase
     last_login: DataTypes.DATE,
-    preference: DataTypes.INTEGER,
+    preference: DataTypes.ENUM("M", "F"),
   });
   return Match;
 }
